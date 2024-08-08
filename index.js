0e0793ec197ec63bd2dc05f54726ad29d57eaf6c
banyaigabor <banyai.gabor@promir.hu>
2024-08-08 16:33:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function formatDate(date) {
 
 // Function to run PowerShell script
 async function runPowerShellScript(taskId, customFieldId, asanaAccessToken) {
-  const ps = new NodePowershell({
+  const ps = new Powershell({
     executionPolicy: 'Bypass',
     noProfile: true
   });
